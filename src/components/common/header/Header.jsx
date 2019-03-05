64d11c5d2c9c27aed47d91440c49023c4a590c18
Isaac <garciaisaac468@gmail.com>
2019-03-04 18:32:28 -0600

    Modify header component and create NavBar with react-router-dom

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './header.scss'
+import {NavLink} from 'react-router-dom'
 
 const Header = (props) => (
-    <h2>Header</h2>
+  <div className="head">
+    <ul id="navbar">
+      <NavLink to="/" className="link">
+        <li className="itemNav">Home</li>
+      </NavLink>
+      <NavLink to="/nosotros/" className="link">
+        <li className="itemNav">Información</li>
+      </NavLink>
+      <NavLink to="/tramites/" className="link">
+        <li className="itemNav">Servicios</li>
+      </NavLink>
+      <NavLink to="/contacto/" className="link">
+        <li className="itemNav">Contacto</li>
+      </NavLink>
+      <NavLink to="/tramites/:slug/" className="link">
+        <li className="itemNav">Servicio</li>
+      </NavLink>
+      <li></li>
+    </ul>
+  </div>
 )
 
 export default Header
