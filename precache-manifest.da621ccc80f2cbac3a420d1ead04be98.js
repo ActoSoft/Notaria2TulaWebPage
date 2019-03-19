919c6baea03c5fee80634c309a0837b97ef2c787
Martin Melo <nitram-210397@hotmail.com>
2019-03-19 15:19:37 -0600

    Updates

---
+++ precache-manifest.da621ccc80f2cbac3a420d1ead04be98.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2119b694f4a3827cc37cb01b172c31",
+    "url": "/Notaria2TulaWebPage/static/media/Sign.da2119b6.jpg"
+  },
+  {
+    "revision": "38341c73cdee53720194",
+    "url": "/Notaria2TulaWebPage/static/css/main.c2f4f7a7.chunk.css"
+  },
+  {
+    "revision": "a8768195917a920252b5",
+    "url": "/Notaria2TulaWebPage/static/js/runtime~main.a8768195.js"
+  },
+  {
+    "revision": "c3c38fa1faf894b6a968",
+    "url": "/Notaria2TulaWebPage/static/js/2.c3c38fa1.chunk.js"
+  },
+  {
+    "revision": "770e7eb669497319b9f6b9141debb16b",
+    "url": "/Notaria2TulaWebPage/static/media/Back1.770e7eb6.jpg"
+  },
+  {
+    "revision": "80c5f3e4f5177daf25c5cda4f0f402c6",
+    "url": "/Notaria2TulaWebPage/static/media/Back2.80c5f3e4.jpg"
+  },
+  {
+    "revision": "de4f4b1038c42bdfccea43b1dd22f0ec",
+    "url": "/Notaria2TulaWebPage/static/media/Back3.de4f4b10.jpg"
+  },
+  {
+    "revision": "38341c73cdee53720194",
+    "url": "/Notaria2TulaWebPage/static/js/main.38341c73.chunk.js"
+  },
+  {
+    "revision": "e56f132e43770d17d1e05a60f68a129a",
+    "url": "/Notaria2TulaWebPage/static/media/clock.e56f132e.svg"
+  },
+  {
+    "revision": "cfc961043df8588da4e13d60d56876c5",
+    "url": "/Notaria2TulaWebPage/static/media/email.cfc96104.svg"
+  },
+  {
+    "revision": "9b025971ea5fb0580a136b1b05129a7a",
+    "url": "/Notaria2TulaWebPage/static/media/phone.9b025971.svg"
+  },
+  {
+    "revision": "bb8d556a6c2adcf644dd460f86f74785",
+    "url": "/Notaria2TulaWebPage/static/media/placeholder-map.bb8d556a.svg"
+  },
+  {
+    "revision": "8a94c9c0fc70e7c3aef85c46225c7020",
+    "url": "/Notaria2TulaWebPage/static/media/Logo_transparencia.8a94c9c0.png"
+  },
+  {
+    "revision": "428dd1a43469b100154caf697e172582",
+    "url": "/Notaria2TulaWebPage/static/media/placeInMap.428dd1a4.png"
+  },
+  {
+    "revision": "a666d3e341dc4d811c6e61edb55489f5",
+    "url": "/Notaria2TulaWebPage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2153b202a41ba6a03f5fba174950d4e.js"
+  "/Notaria2TulaWebPage/precache-manifest.da621ccc80f2cbac3a420d1ead04be98.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Notaria2TulaWebPage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
