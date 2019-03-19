142dc70eaa603da4c1e0da623e162da29e8fc126
Martin Melo <nitram-210397@hotmail.com>
2019-03-19 12:43:10 -0600

    Cleaning methods

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -18,23 +18,16 @@ class HomePage extends Component {
           back2,
           back3
         ],
-        cont: 1,
+        cont: 0,
         interval: null,
+        actualImage: null
       }
   }
 
   componentDidMount = () =>{
     let { interval } = this.state;
     interval = window.setInterval(this.change, 5000);
-
-    let container=document.getElementById('sliderContainer')
-    let img = document.createElement("img")
-
-    img.setAttribute('src', this.state.urls[0])
-    img.setAttribute('id', "imgC")
-    container.appendChild(img)
-
-    this.setState({ interval });
+    this.setState({ interval, actualImage: this.state.urls[0] });
   }
 
   componentWillUnmount = () => {
@@ -44,45 +37,34 @@ class HomePage extends Component {
   }
 
   change = () =>{
-    let { cont } = this.state;
-    let image=document.getElementById('imgC');
-    //image ? alert("si hay algo") :  alert("no hay nada")
-    let container=document.getElementById('sliderContainer');
-
-    image ? container.removeChild(image) : console.log("no hay");
-    if(cont >= 3)
-      cont = 0
-    else if(cont < 0)
-      cont = 2
-    let newImg = document.createElement("img");
-    newImg.setAttribute('src', this.state.urls[cont]);
-    newImg.setAttribute('id', "imgC");
-
-    image ? container.appendChild(newImg) : console.log("no hay2");
-    cont=cont+1;
-    this.setState({cont});
+    let { cont } = this.state
+    cont++
+    if(cont > 2) cont = 0
+    this.setState({cont, actualImage: this.state.urls[cont]});
   }
 
   goKnowUs = () => {
     let top = document.getElementById('abCont').offsetTop
-    console.log(top)
     let scrollStep = top / (1000/30)
     let counter = window.scrollY
-    let scrollInterval = setInterval(()=>{
-      if(counter<=top){
-       // console.log(window.innerHeight)
-        window.scrollBy(0, scrollStep)
-        counter+=scrollStep
-      }else{
-        clearInterval(scrollInterval)
-      }
-    }, 30)
+    let scrollInterval = setInterval(
+      ()=>{
+        if(counter<=top){
+          window.scrollBy(0, scrollStep)
+          counter+=scrollStep
+        }
+        else{
+          clearInterval(scrollInterval)
+        }
+        }, 30
+      )
   }
 
   render(){
     return(
       <div id="homeContent">
         <Slider 
+          actualImage={this.state.actualImage}
           goKnowUs={this.goKnowUs}
         />
 
--- src/components/homepage/Slider.jsx
+++ src/components/homepage/Slider.jsx
@@ -5,6 +5,12 @@ const Slider = (props) => (
         <div id="black"></div>
         <p id="place">Notaría 2 - Tula, Hgo.</p>
         <button id="btnConocenos" onClick={props.goKnowUs}>Conócenos</button>
+        {
+            props.actualImage ? 
+            <img src={props.actualImage} alt="cover" id="imgC"/>
+            : null
+        }
+        
     </div>
 )
 export default Slider
