683fc71dfc78e8b222708f5a21d35c2b25937db7
Isaac <garciaisaac468@gmail.com>
2019-03-28 21:17:44 -0600

    Contact page desktop finished with responsive without programing

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,22 +1,50 @@
 import React, { Component } from 'react'
+import contact from '../../static/assets/Contact.jpg'
+import avatar from '../../static/assets/Icons/user.svg'
+import envelope from '../../static/assets/Icons/envelope.svg'
+import smartphone from '../../static/assets/Icons/smartphone.svg'
 import './contact.scss'
 
 class Contact extends Component {
-    
+
     constructor(props){
         super(props)
-        this.state = {
-            "dummie": "Hola"
-        }
-    }
-
-    componentDidMount = () => {
-
+        this.state = {}
     }
 
     render() {
         return (
-            <p>{this.state.dummie}</p>
+            <div>
+              <div id="contactPresentation">
+                <div className="white"></div>
+                <p className="place">contacto</p>
+                <img src={contact} alt="Contacto" id="contactImage"/>
+              </div>
+              <div className="formPart">
+                <div id="whiteShadow" className="box"></div>
+                <div id="blueBox" className="box">
+                  <p>Déjanos un comentario, nustro personal se pondra en contacto contigo para darle seguimiento a tu caso</p>
+                  <div className="formContent">
+                    <div className="characteristicsClient">
+                      <img src={avatar} alt="avatarIcon"/>
+                      <input type="text" placeholder="Nombre Completo"/>
+                    </div>
+                    <div className="characteristicsClient">
+                      <img src={envelope} alt="emailIcon"/>
+                      <input type="text" placeholder="Correo Electrónico"/>
+                    </div>
+                    <div className="characteristicsClient">
+                      <img src={smartphone} alt="phoneIcon"/>
+                      <input type="text" placeholder="Teléfono de contacto"/>
+                    </div>
+                    <div id="contentTextArea">
+                      <textarea placeholder="Cuéntanos un poco más de como podemos ayudarte" required ></textarea>
+                    </div>
+                  </div>
+                  <button>enviar</button>
+                </div>
+              </div>
+            </div>
         )
     }
 
