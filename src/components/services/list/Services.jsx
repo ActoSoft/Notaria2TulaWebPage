2b110ca19b6794d0ff49b79af43fe02627653997
Martin Melo <nitram-210397@hotmail.com>
2019-03-30 13:25:55 -0600

    Info tramites on state

--- src/components/services/list/Services.jsx
+++ src/components/services/list/Services.jsx
@@ -6,13 +6,58 @@ class Services extends Component {
     constructor(props){
         super(props)
         this.state = {
-            "dummie": "services"
+            data: [
+                {
+                    categoria: "Transmisiones de propiedad/bienes inmuebles",
+                    tramites: [
+                        "Jornada Notarial",
+                        "Compraventa",
+                        "Donación",
+                        "Adjudicación por herencia",
+                        "Constitución de condominio",
+                        "Cancelación de hipoteca",
+                        "Fideicomiso"
+                    ]
+                },
+                {
+                    categoria: "Sucesiones",
+                    tramites: [
+                        "Adjudicación por herencia",
+                        "Asesoría Jurídica",
+                        "Cesión de derechos hereditarios"
+                    ]
+                },
+                {
+                    categoria: "Constitución de sociedades",
+                    tramites: [
+                        "Sociedad Anónima de Capital Variable",
+                        "Sociedad de Responsabilidad Limitada",
+                        "Sociedad Anónima Promotora de Inversión",
+                        "Sociedad Civil",
+                        "Asociación Civil"
+                    ]
+                },
+                {
+                    categoria: "Testamento",
+                    tramites: [
+                        "Spetiembre y Octubre, mes del testamento"
+                    ]
+                },
+                {
+                    categoria: "Trámites diversos",
+                    tramites: [
+                        "Sociedad Anónima de Capital Variable",
+                        "Sociedad de Responsabilidad Limitada",
+                        "Sociedad Anónima Promotora de Inversión",
+                        "Sociedad Civil",
+                        "Asociación Civil"
+                    ]
+                },
+            ],
+            idcategoriaActual: 0
         }
     }
 
-    componentDidMount = () => {
-
-    }
 
     render() {
         return (
