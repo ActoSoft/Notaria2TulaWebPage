77db00452baf9fa222a7cf8990ee0c95dd452236
Isaac <garciaisaac468@gmail.com>
2019-04-10 15:39:33 -0500

    changes in header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -10,22 +10,29 @@ class Header extends Component{
   }
 
   changePosition = () =>{
+    let height=window.innerHeight;
     let y=window.scrollY
     let header=document.getElementById("head")
     let logo=document.getElementById("headerLogo")
     let nav=document.getElementById("navbar")
-    if(y>10){
+    //let options=document.getElementsByClassName("itemNav")
+    if(y>height){
       header.className="onChangeHeader"
       logo.className="changeLogo"
       nav.className="changeNav"
 
       header.style.position="fixed"
       header.style.backgroundColor="#fff"
-      header.style.height="50px"
+      header.style.height="10vh"
+      header.style.webkitBoxShadow="0 0 3px rgb(0,0,0,0.21)"
 
       logo.style.height="40px"
 
       nav.style.padding="0"
+
+      /*for(let i=0;i<options.length;i++){
+        options[i].style.color="#fff"
+      }*/
     }else{
       header.className="onOriginalHeader"
       logo.className="originalLogo"
@@ -33,11 +40,16 @@ class Header extends Component{
 
       header.style.position="absolute"
       header.style.backgroundColor="transparent"
-      header.style.height="90px"
+      header.style.height="10vh"
+      header.style.webkitBoxShadow="none"
 
       logo.style.height="80px"
 
       nav.style.padding="20px 0 0 0"
+
+      /*for(let i=0;i<options.length;i++){
+        options[i].style.color="#000"
+      }*/
     }
   }
 
