ff30d4c1067a87e18718a5647249eeae37abe801
Martin Melo <nitram-210397@hotmail.com>
2019-04-18 13:34:08 -0500

    Notaría acento y cursor pointer en lo circulitos de trámites de la homepage

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 const About = (props) => (
     <div className="aboutContainer" id="abCont">
-        <p className="fullPlace">Notaria Pública #2 del Distrito Judicial de Tula
+        <p className="fullPlace">Notaría Pública #2 del Distrito Judicial de Tula
         de Allende, Hidalgo, y del Patrimonio Inmobiliario Federal</p>
         <div className="descContainerHome">
         <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
